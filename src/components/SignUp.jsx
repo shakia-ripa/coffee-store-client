4392f71c2a91db3ba1fed1b0546fc663f5433805
shakia-ripa <shakiaripa@gmail.com>
2023-10-15 12:06:33 +0600

    created AuthProvider

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+
+    const handleSignUp = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
     return (
-        <div>
-            <h2 className="text-3xl">Please Sign Up</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign up now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email"
+                            name="email"
+                            className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password"
+                            name="password"
+                            className="input input-bordered" required />
+                        </div>
+                        <p>Already have an account? <Link to={'/signin'}>
+                        <span>Sign In</span></Link></p>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
